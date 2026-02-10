b86642930b3f26c95ea007d58aed5a3b1ce93354
brunomarchasson <brunomarchasson@users.noreply.github.com>
2026-02-10 18:39:56 +0100

    aa

--- web/src/migrations/index.ts
+++ web/src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260207_155455_initial_schema from './20260207_155455_initial_schema';
+import * as migration_20260210_173527_add_media_sizes from './20260210_173527_add_media_sizes';
 
 export const migrations = [
   {
     up: migration_20260207_155455_initial_schema.up,
     down: migration_20260207_155455_initial_schema.down,
     name: '20260207_155455_initial_schema',
+  },
+  {
+    up: migration_20260210_173527_add_media_sizes.up,
+    down: migration_20260210_173527_add_media_sizes.down,
+    name: '20260210_173527_add_media_sizes'
   },
 ];
